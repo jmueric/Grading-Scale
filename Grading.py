1c658bde33497a190a41dca9e16c3d2de9995ea5
jmueric <59981920+jmueric@users.noreply.github.com>
2020-05-07 20:03:39 -0400

    Add files via upload

---
+++ Grading.py
@@ -0,0 +1,15 @@
+#Eric Sander
+
+grade = int(input("What is your grade?: "))
+
+
+if grade >= 90:
+    print("A")
+elif grade >= 80:
+    print("B")
+elif grade >= 70:
+    print("C")
+elif grade >= 60:
+    print("D")
+else:
+    print("FAIL!")
